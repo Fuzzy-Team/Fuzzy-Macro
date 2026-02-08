ab8f7bf0fe7e29e754181c630fe62b281494be3c
Logan <95306463+LaganYT@users.noreply.github.com>
2026-02-07 21:26:50 -0600

    Refactor sleep calls and improve objective parsing logic in Brown Bear quest

--- paths/quests/brown bear.py
+++ paths/quests/brown bear.py
@@ -1,15 +1,14 @@
 self.runPath("collect/stockings")
 self.keyboard.keyDown("a")
-self.sleep(1)
+sleep(1)
 self.keyboard.press(" ")
-self.sleep(2)
+sleep(2)
 self.keyboard.keyUp("a")
 
 # slowly walk back until e is visible again
 for _ in range(10):
-    if self.checkForImage("collect/e_icon.png", confidence=0.8):
-        self.keyboard.press("e")
+    if self.isBesideE(["talk", "brown"]):
         break
     self.keyboard.keyDown("d")
-    self.sleep(0.1)
+    sleep(0.1)
     self.keyboard.keyUp("d")
