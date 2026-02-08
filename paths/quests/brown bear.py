f73c18528c0e016942c51d27f35ab1091d2eafcf
Logan <95306463+LaganYT@users.noreply.github.com>
2026-02-07 21:38:41 -0600

    Add dialog interaction for Brown Bear quest when near NPC

--- paths/quests/brown bear.py
+++ paths/quests/brown bear.py
@@ -8,6 +8,11 @@ self.keyboard.keyUp("a")
 # slowly walk back until e is visible again
 for _ in range(10):
     if self.isBesideE(["talk", "brown"]):
+        self.keyboard.press("e")
+        sleep(0.2)
+        self.keyboard.press("e")
+        sleep(0.5)
+        self.clickdialog()
         break
     self.keyboard.keyDown("d")
     sleep(0.1)
