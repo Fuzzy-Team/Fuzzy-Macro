6145fe76985e67128b3e1ce4562362fb1ac42c25
Logan <95306463+LaganYT@users.noreply.github.com>
2026-02-07 21:11:44 -0600

    Fix casing for keyboard keyDown and keyUp methods in Brown Bear quest logic

--- paths/quests/brown bear.py
+++ paths/quests/brown bear.py
@@ -1,15 +1,15 @@
 self.runPath("collect/stockings")
-self.keyboard.keydown("a")
+self.keyboard.keyDown("a")
 self.sleep(1)
 self.keyboard.press(" ")
 self.sleep(2)
-self.keyboard.keyup("a")
+self.keyboard.keyUp("a")
 
 # slowly walk back until e is visible again
 for _ in range(10):
     if self.checkForImage("collect/e_icon.png", confidence=0.8):
         self.keyboard.press("e")
         break
-    self.keyboard.keydown("d")
+    self.keyboard.keyDown("d")
     self.sleep(0.1)
-    self.keyboard.keyup("d")
+    self.keyboard.keyUp("d")
