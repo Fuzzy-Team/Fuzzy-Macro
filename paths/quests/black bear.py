28d495e531b7386059d3bae8ea6baf2290b5ed42
Logan <95306463+LaganYT@users.noreply.github.com>
2026-02-15 23:01:32 -0600

    Fix keyboard press method for black bear interaction and enhance dialog handling

--- paths/quests/black bear.py
+++ paths/quests/black bear.py
@@ -8,6 +8,11 @@ sleep(2)
 self.keyboard.keyUp("s")
 self.keyboard.press("w", 1)
 for _ in range(5):
-    self.keyboard.keyPress("a", 0.1)
+    self.keyboard.press("a", 0.1)
     if self.isBesideE(["talk", "black"]):
+        self.keyboard.press("e")
+        sleep(0.2)
+        self.keyboard.press("e")
+        sleep(0.5)
+        self.clickdialog()
         break
