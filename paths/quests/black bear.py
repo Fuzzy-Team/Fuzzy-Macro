0a56bb35154f23a35cee3397842f8b87d79d1584
Logan <95306463+LaganYT@users.noreply.github.com>
2026-02-15 23:22:10 -0600

    Remove redundant dialog interaction steps for black bear quest

--- paths/quests/black bear.py
+++ paths/quests/black bear.py
@@ -10,9 +10,4 @@ self.keyboard.press("w", 1)
 for _ in range(5):
     self.keyboard.press("a", 0.1)
     if self.isBesideE(["talk", "black"]):
-        self.keyboard.press("e")
-        sleep(0.2)
-        self.keyboard.press("e")
-        sleep(0.5)
-        self.clickdialog()
         break
