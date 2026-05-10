d974fbca96b956cdad8df774e3bcf8f3827276fa
Logan <95306463+LaganYT@users.noreply.github.com>
2026-05-09 22:15:48 -0500

    feat: Refactor cup.py for improved movement logic and alignment handling

--- settings/defaults/patterns/cup.py
+++ settings/defaults/patterns/cup.py
@@ -2,129 +2,159 @@
 # Original Lua pattern by Souka/Seto, based on Kettle by Dully.
 # Converted for Fuzzy Macro's Python pattern runner.
 
-digi_stops = False
+alignment = 0
 
-if sizeword.lower() == "xs":
-    size = 0.25
-elif sizeword.lower() == "s":
-    size = 0.5
-elif sizeword.lower() == "l":
-    size = 1.5
-elif sizeword.lower() == "xl":
-    size = 2
-else:
-    size = 1
+crFDC = 0.3 * 4 + alignment
+dwAlign = 1.5 * 4 + alignment
+hwAlign = 2 * 4 + alignment
 
-step = 0.5
-l = step * size
+l = 6.25 * 4
 s = l / 4
 h = l / 2
 
-# Alignment values are scaled to Fuzzy Macro movement units.
-crfdc = 0.3 * step * size
-dwalign = 1.5 * step * size
-hwalign = 2 * step * size
-align_drift = 0.25 * step * size
+digiStops = False
 
-
-def ds(seconds):
-    if digi_stops:
-        sleep(seconds)
-
+def ds(ms):
+    if digiStops:
+        sleep(ms / 1000)
 
 def dy_walk(length, dir1, dir2=None):
+    tiles = length / 4
     if dir2 is None:
-        self.keyboard.walk(dir1, length)
+        self.keyboard.tileWalk(dir1, tiles)
     else:
-        self.keyboard.multiWalk([dir1, dir2], length)
+        self.keyboard.multiTileWalk([dir1, dir2], tiles)
 
+def set_yaw_7():
+    self.keyboard.press(rotleft)
 
-current_yaw = 0
+def set_yaw_6():
+    self.keyboard.press(rotleft)
+    self.keyboard.press(rotleft)
 
+def set_yaw_0_from_7():
+    self.keyboard.press(rotright)
 
-def set_yaw(yaw):
-    global current_yaw
-    left_turns = (current_yaw - yaw) % 8
-    right_turns = (yaw - current_yaw) % 8
-    if left_turns <= right_turns:
-        for _ in range(left_turns):
-            self.keyboard.press(rotleft)
-    else:
-        for _ in range(right_turns):
-            self.keyboard.press(rotright)
-    current_yaw = yaw
-    sleep(0.05)
+def set_yaw_0_from_6():
+    self.keyboard.press(rotright)
+    self.keyboard.press(rotright)
 
+# shape one (straight)
 
-self.keyboard.press(rotup)
-self.keyboard.press(rotup)
-self.keyboard.press(rotup)
-self.keyboard.press(rotup)
-sleep(0.05)
-
-# Shape one, straight.
 dy_walk(h, leftkey)
+sleep(0.02)
 dy_walk(s, fwdkey)
+sleep(0.02)
 dy_walk(l, rightkey)
+sleep(0.02)
 dy_walk(s, fwdkey)
+sleep(0.02)
 dy_walk(l, leftkey)
-set_yaw(7)
+sleep(0.02)
+
+set_yaw_7()
 dy_walk(l, backkey, leftkey)
-if dwalign > 0:
-    dy_walk(dwalign + align_drift, backkey, leftkey)
-    dy_walk(dwalign, fwdkey, rightkey)
-    ds(0.7)
+sleep(0.02)
+
+if dwAlign > 0:
+    dy_walk(dwAlign + 2, backkey, leftkey)
+    sleep(0.02)
+    dy_walk(dwAlign, fwdkey, rightkey)
+    sleep(0.02)
+    ds(700)
+
 dy_walk(s, backkey, rightkey)
+sleep(0.02)
 dy_walk(l, fwdkey, rightkey)
+sleep(0.02)
 dy_walk(s, backkey, rightkey)
+sleep(0.02)
 dy_walk(h, backkey, leftkey)
-set_yaw(0)
+sleep(0.02)
+
+set_yaw_0_from_7()
 dy_walk(h, backkey)
+sleep(0.02)
 dy_walk(s, rightkey)
+sleep(0.02)
 dy_walk(l, fwdkey)
+sleep(0.02)
 dy_walk(s, rightkey)
-if hwalign > 0:
-    dy_walk(hwalign + align_drift, rightkey)
-    dy_walk(hwalign, leftkey)
-    ds(0.6)
+sleep(0.02)
+
+if hwAlign > 0:
+    dy_walk(hwAlign + 2, rightkey)
+    sleep(0.02)
+    dy_walk(hwAlign, leftkey)
+    sleep(0.02)
+    ds(600)
+
 dy_walk(l, backkey)
-set_yaw(6)
+sleep(0.02)
+
+set_yaw_6()
 dy_walk(l, fwdkey)
+sleep(0.02)
 dy_walk(s, rightkey)
+sleep(0.02)
 dy_walk(l, backkey)
+sleep(0.02)
 dy_walk(s, rightkey)
+sleep(0.02)
 dy_walk(h, fwdkey)
-set_yaw(0)
+sleep(0.02)
 
-# Shape two, diagonal.
+set_yaw_0_from_6()
+
+# shape two (diago)
+
 dy_walk(h, fwdkey, leftkey)
-ds(0.85)
-dy_walk(s, fwdkey, rightkey)
-dy_walk(l, backkey, rightkey)
-dy_walk(s, fwdkey, rightkey)
-dy_walk(l, fwdkey, leftkey)
-set_yaw(7)
-dy_walk(l, leftkey)
-dy_walk(s + crfdc, backkey)
-dy_walk(l, rightkey)
-dy_walk(s + crfdc, backkey)
-dy_walk(h, leftkey)
-set_yaw(0)
-dy_walk(h, backkey, leftkey)
-dy_walk(s, backkey, rightkey)
-dy_walk(l, fwdkey, rightkey)
-dy_walk(s, backkey, rightkey)
-dy_walk(l, backkey, leftkey)
-set_yaw(6)
-dy_walk(l, fwdkey, rightkey)
-dy_walk(l, backkey, leftkey)
-dy_walk(s, backkey, rightkey)
-ds(0.7)
-dy_walk(h, fwdkey, rightkey)
-set_yaw(0)
+sleep(0.02)
+ds(850)
 
-self.keyboard.press(rotdown)
-self.keyboard.press(rotdown)
-self.keyboard.press(rotdown)
-self.keyboard.press(rotdown)
+dy_walk(s, fwdkey, rightkey)
+sleep(0.02)
+dy_walk(l, backkey, rightkey)
+sleep(0.02)
+dy_walk(s, fwdkey, rightkey)
+sleep(0.02)
+dy_walk(l, fwdkey, leftkey)
+sleep(0.02)
+
+set_yaw_7()
+dy_walk(l, leftkey)
+sleep(0.02)
+dy_walk(s + crFDC, backkey)
+sleep(0.02)
+dy_walk(l, rightkey)
+sleep(0.02)
+dy_walk(s + crFDC, backkey)
+sleep(0.02)
+dy_walk(h, leftkey)
+sleep(0.02)
+
+set_yaw_0_from_7()
+dy_walk(h, backkey, leftkey)
+sleep(0.02)
+dy_walk(s, backkey, rightkey)
+sleep(0.02)
+dy_walk(l, fwdkey, rightkey)
+sleep(0.02)
+dy_walk(s, backkey, rightkey)
+sleep(0.02)
+dy_walk(l, backkey, leftkey)
+sleep(0.02)
+
+set_yaw_6()
+dy_walk(l, fwdkey, rightkey)
+sleep(0.02)
+dy_walk(l, backkey, leftkey)
+sleep(0.02)
+dy_walk(s, backkey, rightkey)
+sleep(0.02)
+ds(700)
+
+dy_walk(h, fwdkey, rightkey)
 sleep(0.05)
+
+set_yaw_0_from_6()
